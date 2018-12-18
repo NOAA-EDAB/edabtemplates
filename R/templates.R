aee0d363188606b12a2b6c7808174f2b19d34cc5
slarge <largesi@gmail.com>
2018-12-18 09:38:04 -0500

    add documentation

---
+++ R/templates.R
@@ -0,0 +1,15 @@
+#' Rmarkdown templates for SOE and other reports. After installing the package,
+#' click on `create new Rmarkdown document` in Rstudio to have access to the templates.
+#' See \url{http://rmarkdown.rstudio.com/developer_document_templates.html} for
+#' more information.
+#'
+#'
+#' @docType package
+#' @name templates
+#' @title Rmarkdown templates
+#' @import knitr
+#' @import rmarkdown
+#' @author Scott Large \email{scott.large@noaa.gov}
+#' @keywords package
+#'
+NULL
